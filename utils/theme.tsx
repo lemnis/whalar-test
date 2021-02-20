dfeada8c101c5dd41c6ffb9f4a665f2b95d319bf
Lisa Martens <lemnis.martens@gmail.com>
2021-02-20 14:35:17 +0100

    Setup the theme

---
+++ utils/theme.tsx
@@ -0,0 +1,17 @@
+import indigo from '@material-ui/core/colors/indigo';
+import pink from '@material-ui/core/colors/pink';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: indigo,
+    secondary: pink,
+    background: {
+      default: 'green',
+      paper: 'red'
+    }
+  },
+});
+
+export default theme;
